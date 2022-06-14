5c00685c0b38c32600e9f32e47c35b509a5ec6ee
dincohen92 <dincohen92@gmail.com>
2022-06-14 12:30:13 -0700

    fix products query

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,26 +1,27 @@
 var pool = require('../db/db.js');
 
 getProducts = function(req, res) {
-  let count = req.query.count || 5;
-  let page = req.query.page * count - 1 || 0;
+  let count = parseInt(req.query.count) || 5;
+  let page = parseInt(req.query.page) || 1;
+  console.log(typeof count, page)
   console.log(`request url:: http://localhost:8080/products?page=${page}&count=${count}`)
   const query = {
     text:`
     SELECT id, name, slogan, description, category, default_price
     FROM product
-    LIMIT $2
-    OFFSET $1;
+    ORDER BY id
+    LIMIT $2 OFFSET $1;
     `,
-    values: [page, count]
+    values: [((page * count) - count), count]
   }
   pool.query(query)
-  .then((data) => {
-    res.status(200).send(data.rows)
-  })
-  .catch((err) => {
-    console.log(err)
-    res.status(401).send('could not get products')
-  })
+    .then((data) => {
+      res.status(200).send(data.rows)
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(401).send('could not get products')
+    })
 }
 
 module.exports = getProducts;
